42c772cacea8533cdf215d62618f85ef32acdd73
osvaldodmvs <osvaldodmvs@gmail.com>
2023-04-11 14:19:02 +0100

    added snippet to test simple timing of http request

--- pi/go/main.go
+++ pi/go/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"database/sql"
+	//"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -15,35 +15,44 @@ type SensorData struct {
 }
 
 func main() {
-	connStr := "host=localhost port=5432 user=postgres password=postgres dbname=testingdb sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	log.Printf("Connection to DB estabilished")
-	stmt, err := db.Prepare("INSERT INTO readings (temp, humidity) VALUES ($1, $2)")
-	if err != nil {
-		log.Fatal(err)
-	}
+	/* 	connStr := "host=localhost port=5432 user=postgres password=postgres dbname=testingdb sslmode=disable"
+	   	db, err := sql.Open("postgres", connStr)
+	   	if err != nil {
+	   		log.Fatal(err)
+	   	}
+	   	defer db.Close()
+	   	log.Printf("Connection to DB estabilished")
+	   	stmt, err := db.Prepare("INSERT INTO readings (temp, humidity) VALUES ($1, $2)")
+	   	if err != nil {
+	   		log.Fatal(err)
+	   	}
+	   	log.Printf("Up and running, listening on port 8080")
+	   	http.HandleFunc("/sensor-data", func(w http.ResponseWriter, r *http.Request) {
+	   		if r.Method == "POST" {
+	   			var sensorData SensorData
+	   			err := json.NewDecoder(r.Body).Decode(&sensorData)
+	   			if err != nil {
+	   				http.Error(w, err.Error(), http.StatusBadRequest)
+	   				return
+	   			}
+	   			log.Printf("Received sensor data: %+v", sensorData)
+	   			_, err = stmt.Exec(sensorData.Temperature, sensorData.Humidity)
+	   			if err != nil {
+	   				log.Fatal(err)
+	   			}
+	   		} else {
+	   			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	   		}
+	   	}) */
 	log.Printf("Up and running, listening on port 8080")
 	http.HandleFunc("/sensor-data", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
 			var sensorData SensorData
-			err := json.NewDecoder(r.Body).Decode(&sensorData)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
-			}
+			_ = json.NewDecoder(r.Body).Decode(&sensorData)
 			log.Printf("Received sensor data: %+v", sensorData)
-			_, err = stmt.Exec(sensorData.Temperature, sensorData.Humidity)
-			if err != nil {
-				log.Fatal(err)
-			}
 		} else {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
 	})
-
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
