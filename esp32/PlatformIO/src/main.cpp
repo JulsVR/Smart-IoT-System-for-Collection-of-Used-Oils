47cbd81baf99307b536d5e2f3a7903c24782400b
JulesVR <julesvr.official@gmail.com>
2023-05-22 00:21:47 +0100

    Final changes before collecting measurements

--- esp32/PlatformIO/src/main.cpp
+++ esp32/PlatformIO/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <OneWire.h>
 #include <Keypad.h>
 #include <DallasTemperature.h>
@@ -7,8 +8,9 @@
 #include <HTTPClient.h>
 #include "DHT.h"
 #define DHTPIN 14 // GPIO14
-#define ONE_WIRE_BUS 4
+#define ONE_WIRE_BUS 5 
 #define DHTTYPE DHT22 // DHT22 (AM2302)
+DynamicJsonDocument json(256);
 DHT dht(DHTPIN, DHTTYPE);
 
 
@@ -17,26 +19,27 @@ DallasTemperature sensors(&oneWire);
 
 boolean reading = false;
 
-int flowSensorPin = 2;   
+int flowSensorPin = 17;  
 volatile double waterflow;
 unsigned long lastTime = 0;
-unsigned long timerDelay = 1000;
+unsigned long timerDelay = 800;
 unsigned long pulseDuration; 
 
-unsigned short water = 90.0;
-unsigned short food_oil = 10.0;
-unsigned short car_oil = 0.0;
-//float calibrationFactor = 1.44;
+float readings[30];
+
 float lastTotalFlow = 0.0;
 float calibrationFactor =  0.884;
-float before_temperature = 20.0;
+
 unsigned short before_values[2] = {0,0};
+
+const char* brands[11] = {"FULA", "VAQUEIRO", "VEGE", "FRIGI", "MARCA BRANCA", "CASTROL", "ELF", "MOBIL", "LIQUI MOLY", "SHELL", "TOTAL"};
+int brand_atual = 0;
 
 const byte ROWS = 4; 
 const byte COLS = 3;
 const char* ssid = "ufp";
 const char* password = "";
-const char* serverName = "http://10.10.43.113:8080/sensor-data";
+const char* serverName = "http://192.168.1.87:8080/sensor-data";
 
 byte rowPins[ROWS] = {22,25,33,19}; 
 byte colPins[COLS] = {21,23,32}; 
@@ -50,8 +53,18 @@ char keys[ROWS][COLS] = {
 };
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
-
-
+//sensor values to send through http request
+float flowrate;
+float air_temperature;
+float humidity;
+float liq_temperature;
+float turbidity;
+float before_temperature = 20.0;
+unsigned short water = 90;
+unsigned short food_oil = 10;
+unsigned short engine_oil = 0;
+const char* oil_brand = NULL;
+/////////////////////////
 
 void pulseCounter() {
   waterflow +=  1.0 / 450.0;
@@ -61,27 +74,63 @@ void setup() {
   Serial.begin(9600);
   sensors.begin();
   dht.begin();
-  //  gpio_install_isr_service(0);
+  // gpio_install_isr_service(0);
   //pinMode(flowSensorPin, INPUT);
   //digitalWrite(flowSensorPin, HIGH);
   waterflow = 0;
   attachInterrupt(sensorInterrupt, pulseCounter, RISING);
 
-  /*
+  
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
-  Serial.println("Timer set to 1 seconds (timerDelay variable), it will take 1 seconds before publishing the first reading.");
-
+  Serial.printf("\nSucessfully connected to Wifi %s.\n", ssid);
   bool status;
-  */
   
+  
+}
+
+float quicksortPartition(float arr[], int low, int high) {
+  float pivot = arr[high];
+  int i = (low - 1);
+
+  for (int j = low; j <= high - 1; j++) {
+    if (arr[j] <= pivot) {
+      i++;
+      // Swap arr[i] and arr[j]
+      float temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+
+  // Swap arr[i + 1] and arr[high] (pivot)
+  float temp = arr[i + 1];
+  arr[i + 1] = arr[high];
+  arr[high] = temp;
+
+  return (i + 1);
+}
+
+void quicksort(float arr[], int low, int high) {
+  if (low < high) {
+    int partitionIndex = quicksortPartition(arr, low, high);
+    quicksort(arr, low, partitionIndex - 1);
+    quicksort(arr, partitionIndex + 1, high);
+  }
+}
+
+float getMedian(float arr[], int size) {
+  quicksort(arr, 0, size - 1);
+
+  if (size % 2 == 0) {
+    return (arr[size / 2] + arr[(size / 2) - 1]) / 2.0;
+  } else {
+    return arr[size / 2];
+  }
 }
 
 void printTime() {
@@ -89,71 +138,69 @@ void printTime() {
   unsigned long currentSeconds = (currentMillis / 1000) % 60;
   unsigned long currentMinutes = (currentMillis / (1000 * 60)) % 60;
   unsigned long currentHours = (currentMillis / (1000 * 60 * 60)) % 24;
-  Serial.printf("Time : %02lu:%02lu:%02lu :: ", currentHours, currentMinutes, currentSeconds);
+  unsigned long currentMilliseconds = currentMillis % 1000;
+  Serial.printf("Time: %02lu:%02lu:%02lu.%03lu :: ", currentHours, currentMinutes, currentSeconds, currentMilliseconds);
 }
 
-void dht22() {
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
-  Serial.printf("Temperature: %.2f °C, Humidity: %.2f %%", temperature, humidity);
 
+void dht22() {
+  air_temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+  Serial.printf("Temperature: %.2f °C, Humidity: %.2f %%", air_temperature, humidity);
 }
 
 
 void ds18b20() {
   sensors.requestTemperatures(); 
-  float tempC = sensors.getTempCByIndex(0);
-  Serial.printf("   ---   Fluid Temperature: %.2f (°C)", tempC);
+  liq_temperature = sensors.getTempCByIndex(0);
+  Serial.printf("   ---   Fluid Temperature: %.2f (°C)", liq_temperature);
 }
 
 void sen0189() {
-  int sensorValue = analogRead(0);// read the input on analog pin 0:
-  float voltage = sensorValue * (5.0 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  Serial.printf("   ---   Turbidity: %.2f (V)", voltage);
+  int sensorValue = analogRead(39);
+  turbidity = sensorValue * (5.0 / 1024.0);
+  Serial.printf("   ---   Turbidity: %.2f (V)", turbidity);
 }
 
 void yf_s201(){
-  static unsigned long startTime = 0;    // Variable to store the start time
+  for(int i = 0; i < 30; i++){
+
+ 
+  static unsigned long startTime = 0;   
   static unsigned long elapsedTime = 0;
   unsigned long long elapsedSeconds = 0;
   float fixedflowRate = 0;  
   
-  //float flowRate = waterflow * calibrationFactor * 60.0;
   float totalFlow = waterflow * 1000 * calibrationFactor;
   
 
     if (totalFlow != lastTotalFlow) {
     if (startTime == 0) {
-      // Start the timer if it hasn't started already
       startTime = millis();
     } else {
-      // Calculate the elapsed time if the timer is already running
       unsigned long currentTime = millis();
       elapsedTime += currentTime - startTime;
       startTime = currentTime;
     }
     lastTotalFlow = totalFlow;
-  }
-  elapsedSeconds = elapsedTime / 1000;
-  fixedflowRate = ((totalFlow / elapsedSeconds)  * 60.0)/1000.0;
+    }
+    elapsedSeconds = elapsedTime / 1000;
+    fixedflowRate = ((totalFlow / elapsedSeconds)  * 60.0)/1000.0;
 
-  Serial.printf("Flow Rate: %.2f (L/min)\tTotal Flow: %.0f (mL)\tTime: %lu(s)\n", fixedflowRate, totalFlow, elapsedSeconds);
+  readings[i] = fixedflowRate;
+  }
+  flowrate = getMedian(readings, 30);
+  Serial.printf("   ---   Flow Rate: %.2f (L/min)\n", flowrate);
+
 }
 
-
-void sync(){
+void getFlowRate(){
+  Serial.printf(" --------------- MEDIAN = %.2f ", flowrate);
   printTime();
-  dht22();
-  ds18b20();
-  sen0189();
-  yf_s201();
-  Serial.println();
-  delay(1000);
 }
 
 void keypad_func(){
   char key = keypad.getKey();
-  //Serial.printf("%hu, %hu, %hu, %f\n",water,food_oil,car_oil,before_temperature);
   switch(key){
     case '1':
       Serial.printf("key pressed : %c\n",key);
@@ -162,8 +209,8 @@ void keypad_func(){
       }
       water += 1;
       food_oil -= 1;
-      if(car_oil!=0)
-        car_oil=0;
+      if(engine_oil!=0)
+        engine_oil=0;
       break;
     case '2':
       Serial.printf("key pressed : %c\n",key);
@@ -172,8 +219,8 @@ void keypad_func(){
       }
       water -= 1;
       food_oil += 1;
-      if(car_oil!=0)
-        car_oil=0;
+      if(engine_oil!=0)
+        engine_oil=0;
       break;
     case '3':
       Serial.printf("key pressed : %c\n",key);
@@ -181,7 +228,7 @@ void keypad_func(){
       before_values[1] = food_oil;
       water = 0.0;
       food_oil = 0.0;
-      car_oil = 100.0;
+      engine_oil = 100.0;
       break;
     case '4':
       Serial.printf("key pressed : %c\n",key);
@@ -190,6 +237,14 @@ void keypad_func(){
     case '5':
       Serial.printf("key pressed : %c\n",key);
       before_temperature -= 0.5;
+      break;
+    case '6':
+      Serial.printf("key pressed : %c\n",key);
+      brand_atual++;
+      if(brand_atual > 10)
+        brand_atual = 0;
+      oil_brand = brands[brand_atual];
+      Serial.printf("Oil brand = %s\n",oil_brand);
       break;
     case '*':
       reading = true;
@@ -204,41 +259,60 @@ void keypad_func(){
   }
 }
 void http_post(){
-  if ((millis() - lastTime) > timerDelay) {
-    //Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED){
-      WiFiClient client;
-      HTTPClient http;
-    
-      // Your Domain name with URL path or IP address with path
-      http.begin(client, serverName);
+  if(reading == 1){
+    if ((millis() - lastTime) > timerDelay) {
+      //Check WiFi connection status
+      if(WiFi.status()== WL_CONNECTED){
+        WiFiClient client;
+        HTTPClient http;
       
-      http.addHeader("Content-Type", "application/json");
-      int httpResponseCode = http.POST("{\"temperature\":25.0,\"humidity\":50.0}");
-     
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
         
-      // Free resources
-      http.end();
-    }
-    else {
-      Serial.println("WiFi Disconnected");
-    }
-    lastTime = millis();
+        http.begin(client, serverName);
+
+        json["flow"] = flowrate;
+        json["air_temperature"] = air_temperature;
+        json["humidity"] = humidity;
+        json["liq_temperature"] = liq_temperature;
+        json["turbidity"] = turbidity;
+        json["before_temperature"] = before_temperature;
+        json["water"] = water;
+        json["food_oil"] = food_oil;
+        json["engine_oil"] = engine_oil;
+        json["oil_brand"] = oil_brand;
+        String payload;
+        serializeJson(json, payload);
+
+        http.addHeader("Content-Type", "application/json");
+        int httpResponseCode = http.POST(payload);
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+          
+        // Free resources
+        http.end();
+      }
+      else {
+        Serial.println("WiFi Disconnected\n");
+      }
+      lastTime = millis();
+      } 
   }
 }
 
-void loop() {
-  //keypad_func();
-  //http_post();
-  //yf_s201();
+void sync(){
+  printTime();
+  dht22();
+  ds18b20();
+  sen0189();
+  yf_s201();
+  http_post();
+}
+
+void loop() {  
   if(reading==true){
     sync();
     keypad_func();
   }
-  keypad_func();
-  
+  keypad_func();  
 }
 
 
--- pi/go/main.go
+++ pi/go/main.go
@@ -1,65 +1,61 @@
+golang
 package main
 
 import (
-	//"database/sql"
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	_ "github.com/lib/pq"
+
 )
 
 type SensorData struct {
 	//sensor values
-	Out_Temperature float64 `json:"out_temperature"` //dht22
-	Humidity float64 `json:"humidity"` //dht22
-	Temperature float64 `json:"temperature"` //ds18b20
-	Flow float64 `json:"flow"` //yf-s201
-	Turbidity float64 `json:"turbidity"` 
+	Flow            float64 `json:"flow"`            //yf-s201
+	Air_Temperature float64 `json:"air_temperature"` //dht22
+	Humidity        float64 `json:"humidity"`        //dht22
+	Liq_Temperature float64 `json:"liq_temperature"` //ds18b20
+	Turbidity       float64 `json:"turbidity"`
 	//static values inserted by keypad
 	Before_Temperature float64 `json:"before_temperature"` //cooking thermometer
-	Water float64 `json:"water"`
-	Food_Oil float64 `json:"food_oil"`
-	Engine_Oil float64 `json:"engine_oil"`
-	Oil_brand string `json:"oil_brand"`
+	Water              int32   `json:"water"`
+	Food_Oil           int32   `json:"food_oil"`
+	Engine_Oil         int32   `json:"engine_oil"`
+	Oil_brand		   string  `json:"oil_brand"`
+	Timestamp		   time.Time `json:"timestamp"`
 }
 
 func main() {
-	/* 	connStr := "host=localhost port=5432 user=postgres password=postgres dbname=testingdb sslmode=disable"
-	   	db, err := sql.Open("postgres", connStr)
-	   	if err != nil {
-	   		log.Fatal(err)
-	   	}
-	   	defer db.Close()
-	   	log.Printf("Connection to DB estabilished")
-	   	stmt, err := db.Prepare("INSERT INTO readings (temp, humidity) VALUES ($1, $2)")
-	   	if err != nil {
-	   		log.Fatal(err)
-	   	}
-	   	log.Printf("Up and running, listening on port 8080")
-	   	http.HandleFunc("/sensor-data", func(w http.ResponseWriter, r *http.Request) {
-	   		if r.Method == "POST" {
-	   			var sensorData SensorData
-	   			err := json.NewDecoder(r.Body).Decode(&sensorData)
-	   			if err != nil {
-	   				http.Error(w, err.Error(), http.StatusBadRequest)
-	   				return
-	   			}
-	   			log.Printf("Received sensor data: %+v", sensorData)
-	   			_, err = stmt.Exec(sensorData.Temperature, sensorData.Humidity)
-	   			if err != nil {
-	   				log.Fatal(err)
-	   			}
-	   		} else {
-	   			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	   		}
-	   	}) */
+	connStr := "host=localhost port=5432 user=postgres password=postgres dbname=oau_22_23 sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	log.Printf("Connection to DB estabilished")
+	stmt, err := db.Prepare("INSERT INTO readings (flow,air_temperature,humidity,liq_temperature,turbidity,before_temperature,water,food_oil,engine_oil,oil_brand,timestamp) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)")
+	if err != nil {
+		log.Fatal(err)
+	}
 	log.Printf("Up and running, listening on port 8080")
 	http.HandleFunc("/sensor-data", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
 			var sensorData SensorData
-			_ = json.NewDecoder(r.Body).Decode(&sensorData)
+			err := json.NewDecoder(r.Body).Decode(&sensorData)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				log.Fatal(err)
+				return
+			}
+			sensorData.Timestamp=time.Now().UTC()
 			log.Printf("Received sensor data: %+v", sensorData)
+			_, err = stmt.Exec(sensorData.Flow, sensorData.Air_Temperature, sensorData.Humidity, sensorData.Liq_Temperature, sensorData.Turbidity, sensorData.Before_Temperature, sensorData.Water, sensorData.Food_Oil, sensorData.Engine_Oil, sensorData.Oil_brand, sensorData.Timestamp)
+			if err != nil {
+				log.Fatal(err)
+			}
 		} else {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
