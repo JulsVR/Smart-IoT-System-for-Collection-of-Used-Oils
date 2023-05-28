84927416cf79c2a161ef5e683f63f35cec2ce995
JulesVR <julesvr.official@gmail.com>
2023-05-28 03:16:43 +0100

    App Update
    App "completed" missing integration with the API. Will re-design some parts too.

--- app/lpi_app/lib/main.dart
+++ app/lpi_app/lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+//Global variables to fetch data from the API:
+double temperature_f = 0.0;
+double water_f = 0.0;
+double food_oil_f = 0.0;
+double impurities_f = 10.0;
 
 void main() {
   runApp(MyApp());
@@ -81,9 +88,14 @@ class PercentagesMenu extends StatelessWidget {
         itemBuilder: (context, index) {
           return OutlinedButton(
               onPressed: () {
+                double selectedWater =
+                    double.parse(percentages[index].split('%')[0]);
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => TemperatureMenu()),
+                  MaterialPageRoute(
+                      builder: (context) => TemperatureMenu(
+                            water: selectedWater,
+                          )),
                 );
               },
               style: OutlinedButton.styleFrom(
@@ -107,13 +119,16 @@ class PercentagesMenu extends StatelessWidget {
 }
 
 class TemperatureMenu extends StatelessWidget {
-  final List<String> percentages = [
+  final List<String> temperatures = [
     '20% ºC',
     '25% ºC',
     '30% ºC',
     '35% ºC',
     '40% ºC',
   ];
+  double? water;
+
+  TemperatureMenu({this.water});
 
   @override
   Widget build(BuildContext context) {
@@ -123,10 +138,22 @@ class TemperatureMenu extends StatelessWidget {
         backgroundColor: Color.fromARGB(255, 14, 136, 45),
       ),
       body: ListView.builder(
-        itemCount: percentages.length,
+        itemCount: temperatures.length,
         itemBuilder: (context, index) {
           return OutlinedButton(
-              onPressed: () {},
+              onPressed: () {
+                double selectedTemperature =
+                    double.parse(temperatures[index].split('%')[0]);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => OAUDetails(
+                            temp_x: selectedTemperature,
+                            water_x: water!,
+                            food_oil_x: 100.0 - water!,
+                          )),
+                );
+              },
               style: OutlinedButton.styleFrom(
                 foregroundColor: Colors.black,
                 side: BorderSide(color: Colors.green, width: 4),
@@ -136,7 +163,7 @@ class TemperatureMenu extends StatelessWidget {
                 children: [
                   Expanded(
                       child: Text(
-                    percentages[index],
+                    temperatures[index],
                     textAlign: TextAlign.center,
                   ))
                 ],
@@ -145,4 +172,75 @@ class TemperatureMenu extends StatelessWidget {
       ),
     );
   }
+}
+
+class OAUData {
+  final String label;
+  final double value;
+
+  OAUData(this.label, this.value);
+}
+
+class OAUGraph extends State<OAUDetails> {
+  late List<OAUData> _chartData;
+  late TooltipBehavior _tooltipBehavior;
+
+  List<OAUData> getChartData() {
+    final List<OAUData> chartData = [
+      //OAUData('Temperature', widget.temp_x),
+      OAUData('Water', widget.water_x),
+      OAUData('Food Oil', widget.food_oil_x),
+      OAUData('Impurities', widget.impurities_x),
+    ];
+    //chartData.removeWhere((data) => data.value == 0.0);
+    return chartData;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _chartData = getChartData();
+    _tooltipBehavior = TooltipBehavior(enable: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: Text('OAU')),
+        body: SfCircularChart(
+          title: ChartTitle(
+            text: 'Temperature: ${widget.temp_x.toString()}ºC',
+          ),
+          legend: Legend(
+            isVisible: true,
+            overflowMode: LegendItemOverflowMode.wrap,
+          ),
+          tooltipBehavior: _tooltipBehavior,
+          series: <CircularSeries>[
+            PieSeries<OAUData, String>(
+              dataSource: _chartData,
+              xValueMapper: (OAUData data, _) => data.label,
+              yValueMapper: (OAUData data, _) => data.value,
+              dataLabelSettings: DataLabelSettings(isVisible: true),
+              enableTooltip: true,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OAUDetails extends StatefulWidget {
+  double temp_x = temperature_f;
+  double water_x = water_f;
+  double food_oil_x = food_oil_f;
+  double impurities_x = impurities_f;
+
+  OAUDetails(
+      {required this.temp_x, required this.water_x, required this.food_oil_x});
+
+  @override
+  OAUGraph createState() => OAUGraph();
 }
